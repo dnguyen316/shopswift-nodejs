11dfeff990139e342b923053b99d4ff7dd599c93
dnguyen316 <ngltduong@gmail.com>
2024-05-01 22:07:21 +0700

    error handling, response handling using class

--- src/services/access.service.js
+++ src/services/access.service.js
@@ -8,6 +8,7 @@ const crypto = require("node:crypto");
 const KeyTokenService = require("./keyToken.service");
 const { createTokenPair } = require("../auth/authUtils");
 const { getInfoData } = require("../utils");
+const { BadRequestError } = require("../core/error.response");
 
 const RoleShop = {
   SHOP: "0001",
@@ -18,104 +19,90 @@ const RoleShop = {
 
 class AccessService {
   static signUp = async ({ name, email, password }) => {
-    try {
-      /* Step1: check email exist?
-       * Using lean() method to optimize performance.
-       * Unless use lean(), it will return mongoose's object and the size would bigger than upto 30 times.
-       * With lean(), it would be return js object
-       */
-      const holderShop = await shopModel.findOne({ email }).lean();
-      if (holderShop) {
-        return {
-          code: "xxxx",
-          message: "Shop already registered!",
-        };
-      }
+    /* Step1: check email exist?
+     * Using lean() method to optimize performance.
+     * Unless use lean(), it will return mongoose's object and the size would bigger than upto 30 times.
+     * With lean(), it would be return js object
+     */
+    const holderShop = await shopModel.findOne({ email }).lean();
+    if (holderShop) {
+      throw new BadRequestError("Error: Shop already registered!");
+    }
 
-      const passwordHash = await bcrypt.hash(password, 10);
-      const newShop = await shopModel.create({
-        name,
-        email,
-        password: passwordHash,
-        roles: [RoleShop.SHOP],
+    const passwordHash = await bcrypt.hash(password, 10);
+    const newShop = await shopModel.create({
+      name,
+      email,
+      password: passwordHash,
+      roles: [RoleShop.SHOP],
+    });
+
+    if (newShop) {
+      // created privateKey, publicKey
+
+      // Public key CryptoGraphy Standards !
+      /**
+       * RSA Algorithm for Sign-up Service
+       * This approach would be use for complex and robust secure in big system such as Google Cloud or Amazon
+       * with this way, we would create the publicKeyString and store it on DB and just only it (doesn't have the private key and the DB as well).
+       * using this method: publicKeyObject = crypto.createPublicKey(publicKeyString) to convert publicKeyObject and then pass it to function createTokenPair instead of normal public key
+       */
+      // const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
+      //   modulusLength: 4096,
+      //   publicKeyEncoding: {
+      //     type: "pkcs1",
+      //     format: "pem",
+      //   },
+      //   privateKeyEncoding: {
+      //     type: "pkcs1",
+      //     format: "pem",
+      //   },
+      // });
+
+      /**
+       * This approach below would be more simplifier for small e-commerce project
+       */
+      const privateKey = crypto.randomBytes(64).toString("hex");
+      const publicKey = crypto.randomBytes(64).toString("hex");
+
+      console.log({ privateKey, publicKey }); //save to collection KeyStore
+      const keyStore = await KeyTokenService.createKeyToken({
+        userId: newShop._id,
+        publicKey,
+        privateKey,
       });
 
-      if (newShop) {
-        // created privateKey, publicKey
-
-        // Public key CryptoGraphy Standards !
-        /**
-         * RSA Algorithm for Sign-up Service
-         * This approach would be use for complex and robust secure in big system such as Google Cloud or Amazon
-         * with this way, we would create the publicKeyString and store it on DB and just only it (doesn't have the private key and the DB as well).
-         * using this method: publicKeyObject = crypto.createPublicKey(publicKeyString) to convert publicKeyObject and then pass it to function createTokenPair instead of normal public key
-         */
-        // const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
-        //   modulusLength: 4096,
-        //   publicKeyEncoding: {
-        //     type: "pkcs1",
-        //     format: "pem",
-        //   },
-        //   privateKeyEncoding: {
-        //     type: "pkcs1",
-        //     format: "pem",
-        //   },
-        // });
-
-        /**
-         * This approach below would be more simplifier for small e-commerce project
-         */
-        const privateKey = crypto.randomBytes(64).toString("hex");
-        const publicKey = crypto.randomBytes(64).toString("hex");
-
-        console.log({ privateKey, publicKey }); //save to collection KeyStore
-        const keyStore = await KeyTokenService.createKeyToken({
-          userId: newShop._id,
-          publicKey,
-          privateKey,
-        });
-
-        if (!keyStore) {
-          return {
-            code: "xxxx",
-            message: "keyStore error",
-          };
-        }
-
-        // const publicKeyObject = crypto.createPublicKey(publicKeyString);
-        // console.log(`publicKeyObject::`, publicKeyObject);
-
-        // created token pair
-        const tokens = await createTokenPair(
-          { userId: newShop._id, email },
-          publicKey,
-          privateKey
-        );
-        console.log(`Created Token Success::`, tokens);
-
-        return {
-          code: 201,
-          metadata: {
-            shop: getInfoData({
-              fields: ["_id", "name", "email"],
-              object: newShop,
-            }),
-            tokens,
-          },
-        };
+      if (!keyStore) {
+        throw new BadRequestError("KeyStore Error");
       }
 
+      // const publicKeyObject = crypto.createPublicKey(publicKeyString);
+      // console.log(`publicKeyObject::`, publicKeyObject);
+
+      // created token pair
+      const tokens = await createTokenPair(
+        { userId: newShop._id, email },
+        publicKey,
+        privateKey
+      );
+      console.log(`Created Token Success::`, tokens);
+
       return {
-        code: 200,
-        metadata: null,
-      };
-    } catch (error) {
-      return {
-        code: "xxx",
-        message: error.message,
-        status: "error",
+        code: 201,
+        metadata: {
+          shop: getInfoData({
+            fields: ["_id", "name", "email"],
+            object: newShop,
+          }),
+          tokens,
+        },
       };
     }
+
+    return {
+      code: 200,
+      metadata: null,
+    };
   };
 }
 
